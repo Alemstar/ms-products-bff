ee73f52c136e89f27f5f5f1cb7707c1b4faa8a86
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-04 03:50:36 -0300

    fixes

--- src/main/java/cl/duoc/ms_products_bff/controller/ProductController.java
+++ src/main/java/cl/duoc/ms_products_bff/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import cl.duoc.ms_products_bff.model.dto.ProductDTO;
 import cl.duoc.ms_products_bff.service.ProductService;
 import feign.FeignException.FeignClientException;
 
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
